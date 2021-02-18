e5d2e995401c08025a660e1c610460b04cc5344a
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-02-18 15:10:51 +0330

    Create en.js

---
+++ en.js
@@ -0,0 +1,13 @@
+  
+var convert = (num) => {
+  if(num == 0){return "one";}
+  if(num == 1){return "یک";}
+  if(num == 2){return "دو";}
+  if(num == 3){return "سه";}
+  if(num == 4){return "چهار";}
+  if(num == 5){return "پنج";}
+  if(num == 6){return "شش";}
+  if(num == 7){return "هفت";}
+  if(num == 8){return "هشت";}
+  if(num = =9){return "نه";}
+}
