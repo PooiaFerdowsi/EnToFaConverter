c7eac5e4487604166dc3d8cd18a002e1b882192f
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-02-22 12:07:29 +0330

    Update ar.js

--- ar.js
+++ ar.js
@@ -4,7 +4,7 @@ function convert = (num) => {
   if(num == 2){return "اثنان";}
   if(num == 3){return "ثلاثة";}
   if(num == 4){return "اربعة";}
-  if(num == 5){return "five";}
+  if(num == 5){return "خمسة";}
   if(num == 6){return "six";}
   if(num == 7){return "seven";}
   if(num == 8){return "eight";}
